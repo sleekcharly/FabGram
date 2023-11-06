0215406fc00a9699e51a081e6b49f40fa735d128
Charles <c_ukasoanya@yahoo.com>
2023-11-06 01:09:16 +0100

    setup tailwind and firebase authentication on login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
+    fill: (theme) => ({
+      red: theme('colors.red.primary'),
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98',
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059',
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb',
+      },
+      red: {
+        primary: '#ed4956',
+      },
+    },
   },
   plugins: [],
 };
